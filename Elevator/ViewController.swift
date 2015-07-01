ef97983d45373d50f83ef918e5386adbde5d4c3d
Jack-Hutchinson <jack_hutchinson@hotmail.com>
2015-06-30 23:24:26 -0700

    Initial Commit

---
+++ Elevator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Elevator
+//
+//  Created by Jack Hutchinson on 6/30/15.
+//  Copyright (c) 2015 Jack Hutchinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
